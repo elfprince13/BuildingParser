a8c6e210ee3b1138e94117fe9a8614d493cddcf1
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2014-11-07 00:47:04 -0500

    Reduce number of sliding window detections
    added option to limit aspect ratios by a thresholded histogram

--- src/edu/brown/cs/buildingparser/detection/SVMDetector.scala
+++ src/edu/brown/cs/buildingparser/detection/SVMDetector.scala
@@ -6,28 +6,64 @@ import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 import org.opencv.core.MatOfFloat
 import edu.brown.cs.buildingparser.Util
+import org.opencv.core.Size
+import org.opencv.core.MatOfPoint
+import org.opencv.core.Rect
+import org.opencv.core.Point
+import scala.collection.SeqView
+import org.apache.commons.io.IOUtils
+import scala.util.Marshal
 
-class SVMDetector(svmName:String, hogsUsed:List[HOGDescriptor]) {
-	val hogBuckets = (hogsUsed.seq.map(hog => hog.get_winSize() -> hog)).toMap
-	val biggestBucket = hogBuckets.keys.maxBy(_.area)
+class SVMDetector(svmName:String, hogsUsed:List[HOGDescriptor], histogramName:String = "", histogramThreshold:Int = 1) {
+	val histogram = if(histogramName != ""){
+		Console.println("Restricting hogsUsed by example-histogram with threshold: " + histogramThreshold)
+		val in = new java.io.FileInputStream(histogramName)
+		Marshal.load[Map[(Double,Double),Int]](IOUtils.toByteArray(in)).map(p => new Size(p._1._1, p._1._2) -> p._2)
+	} else { null }
+	val hogBuckets = (hogsUsed.view.filter(hog => (histogram == null) || 
+			(histogram.getOrElse(hog.get_winSize,0) > histogramThreshold)).map{hog => 
+				if(histogram != null){
+					Console.println("Histogram allowed detector for " + hog.get_winSize + " ( " + histogram(hog.get_winSize) + " > " + histogramThreshold + " )")
+				}
+				hog.get_winSize -> hog}).toMap
+	val maxHog = hogsUsed.maxBy(_.get_winSize.area) // Can't use buckets directly, since we might not have been invited to the party
+	val DETECTION_THRESHOLD = 0.5
 	
 	val mySVM = new CvSVM()
 	mySVM.load(svmName)
 	
 	def detect(inMat:Mat) = {
-		val maxHog = hogBuckets(biggestBucket)
-		val responses = hogsUsed.seq.map{hog => 
-			val smallMat = new Mat
+		val responses = hogBuckets.view.map{case(hogSize, hog) => 
+			//val smallMat = new Mat
 			val hogF = new MatOfFloat
-			Imgproc.resize(inMat, smallMat, hog.get_winSize)
-			hog.compute(smallMat, hogF)
-			(hog.get_winSize -> mySVM.predict(Util.svmFeatureFromHog(hog, maxHog, hogF)))
-		}.toMap
-		responses.foreach{
-			case(size,response) =>
-				Console.println(size + ": " + response)
-		}
+			val locs = new MatOfPoint
+			//Imgproc.resize(inMat, smallMat, hog.get_winSize)
+			val imgSize = inMat.size
+			val borderSize = new Size(hogSize.width / 2, hogSize.height / 2)
+			val cellSize = hog.get_cellSize
+			// might be off by one
+			val widthInCells = ((imgSize.width + cellSize.width).intValue / cellSize.width.intValue)
+			val heightInCells = ((imgSize.height + cellSize.height).intValue / cellSize.height.intValue)
+			hog.compute(inMat, hogF, cellSize, borderSize, locs)
+			val hogLen = hog.getDescriptorSize.intValue
+			val timesComputed = hogF.size.height / hogLen
+			if (widthInCells * heightInCells != timesComputed){
+				throw new IllegalStateException("the sliding window calculation doesn't check out")
+			}
+			Console.println( hogSize + " " + cellSize + " " + borderSize + " " + hogF.size + " " + hogLen + " " + timesComputed)
+			(0 until timesComputed.intValue).view.map{
+				winNum =>
+				val predicted = mySVM.predict(Util.svmFeatureFromHog(hog, maxHog, hogF.submat(winNum*hogLen,(winNum+1)*hogLen,0,1)))
+				val x = (winNum % widthInCells) * cellSize.width
+				val y = (winNum / widthInCells) * cellSize.height
+				/*if(predicted != 0){
+					Console.println("hit: " + x + " " + y + " ( " + predicted + " )")
+				}*/
+				(new Rect(new Point(x,y), hogSize) -> predicted)
+			}.filter(_._2 > DETECTION_THRESHOLD )
+		}.flatten
 		
+		responses
 		
 	}
 }
--- src/edu/brown/cs/buildingparser/training/LearnObjectType.scala
+++ src/edu/brown/cs/buildingparser/training/LearnObjectType.scala
@@ -10,10 +10,14 @@ import org.opencv.highgui.Highgui
 import org.opencv.core.MatOfFloat
 import scala.actors.Actor
 import scala.actors.Channel
+import org.opencv.core.Size
+import scala.util.Marshal
+import scala.io.Source
+import org.apache.commons.io.IOUtils
 
 class LearnObjectType(className:String, objDir:String, objAntiDir:String, counterClasses:List[String],
 		exts:Set[String], hogs:List[HOGDescriptor]) {
-	val hogBuckets = (hogs.seq.map(hog => hog.get_winSize() -> hog)).toMap
+	val hogBuckets = (hogs.map(hog => hog.get_winSize() -> hog)).toMap
 	val biggestBucket = hogBuckets.keys.maxBy(_.area)
 	
 	var mySVM:CvSVM = null
@@ -49,11 +53,11 @@ class LearnObjectType(className:String, objDir:String, objAntiDir:String, counte
 		trainData = Mat.zeros(totalExamples, rowWidth, CvType.CV_32F)
 		responses = Mat.zeros(totalExamples, 1 , CvType.CV_32F)
 		Mat.ones(posCount, 1, CvType.CV_32F).copyTo(responses.submat(0, posCount, 0, 1))
-		Mat.ones(negCount, 1, CvType.CV_32F).mul(Mat.ones(negCount, 1, CvType.CV_32F), -1).copyTo(responses.submat(posCount, totalExamples, 0, 1))
+		//Mat.ones(negCount, 1, CvType.CV_32F).mul(Mat.ones(negCount, 1, CvType.CV_32F), -1).copyTo(responses.submat(posCount, totalExamples, 0, 1))
 		
 		Console.println("Estimate trainData for " + className + " is " + ((trainData.size.area * 4) / (1024 * 1024)).intValue + "MB")
 		Console.println("Copying posExamples for " + className + " ...\t")
-		posExamples.seq.zipWithIndex.foreach{
+		posExamples.view.zipWithIndex.foreach{
 			case(posExample, row) =>
 				val exMat = Highgui.imread(posExample, Highgui.CV_LOAD_IMAGE_GRAYSCALE)
 				val hog = hogBuckets(exMat.size)
@@ -62,7 +66,7 @@ class LearnObjectType(className:String, objDir:String, objAntiDir:String, counte
 				Util.svmFeatureFromHog(hog, maxHog, exHogF).copyTo(trainData.submat(row, row + 1, 0, rowWidth))
 		}
 		Console.println("Copying negExamples for " + className + " ...\t")
-		negExamples.seq.zipWithIndex.foreach{
+		negExamples.view.zipWithIndex.foreach{
 			case(negExample, row) =>
 				val exMat = Highgui.imread(negExample, Highgui.CV_LOAD_IMAGE_GRAYSCALE)
 				val hog = hogBuckets(exMat.size)
@@ -70,6 +74,24 @@ class LearnObjectType(className:String, objDir:String, objAntiDir:String, counte
 				hog.compute(exMat,exHogF)
 				Util.svmFeatureFromHog(hog, maxHog, exHogF).copyTo(trainData.submat(posCount + row, posCount + row + 1, 0, rowWidth))
 		}
+	}
+	
+	def saveExamplesHistogram():Unit = {
+		var hist = Map[Size,Int]()
+		posExamples.foreach{
+			posExample =>
+			val exMat = Highgui.imread(posExample, Highgui.CV_LOAD_IMAGE_GRAYSCALE)
+			val exSize = exMat.size
+			if (hist.contains(exSize) ){
+				hist += (exSize -> (hist(exSize) + 1))
+			} else {
+				hist += (exSize -> 1)
+			}
+		}
+		val out = new java.io.FileOutputStream("trained-svms" + File.separator + className + "-posExSizes.histogram")
+		val serMap = hist.map(p => (p._1.width, p._1.height) -> p._2)
+		IOUtils.write(Marshal.dump(serMap),out)
+		
 	}
 	
 	def clearExamplesLists():Unit = {
@@ -90,9 +112,10 @@ class LearnObjectType(className:String, objDir:String, objAntiDir:String, counte
 
 object LearnerFactory {
 	class Learner(className:String, counterClasses:List[String], usedHogs:List[HOGDescriptor]) extends Actor {
+		val learner = new LearnObjectType(className, "object-classes", "anti-object-classes", counterClasses, Set[String](".png",".jpg",".jpeg"), usedHogs)
 		def act():Unit = {
-			val learner = new LearnObjectType(className, "object-classes", "anti-object-classes", counterClasses, Set[String](".png",".jpg",".jpeg"), usedHogs)
 			learner.initExamplesLists
+			learner.saveExamplesHistogram
 			learner.initTrainingData
 			learner.clearExamplesLists // Save some memory
 			learner.initSVM
@@ -103,21 +126,21 @@ object LearnerFactory {
 		}
 	}
 	
-	def learnDoors(usedHogs:List[HOGDescriptor]) = {
+	def learnDoors(usedHogs:List[HOGDescriptor], autoStart:Boolean = true) = {
 		val doorLearner = new Learner("door", List("window","balcony"), usedHogs)
-		doorLearner.act
+		if(autoStart){doorLearner.start}
 		doorLearner
 	}
 	
-	def learnWindows(usedHogs:List[HOGDescriptor]) = {
+	def learnWindows(usedHogs:List[HOGDescriptor], autoStart:Boolean = true) = {
 		val windowLearner = new Learner("window",List("door","balcony"),usedHogs)
-		windowLearner.act
+		if(autoStart){windowLearner.start}
 		windowLearner
 	}
 	
-	def learnBalconies(usedHogs:List[HOGDescriptor]) = {
+	def learnBalconies(usedHogs:List[HOGDescriptor], autoStart:Boolean = true) = {
 		val balconyLearner = new Learner("balcony", List("door","window"), usedHogs)
-		balconyLearner.act
+		if(autoStart){balconyLearner.start}
 		balconyLearner
 	}
 	
