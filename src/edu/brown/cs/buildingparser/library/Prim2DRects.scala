13d027bb5629874c5d73a61b771303e5cc05677b
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2014-12-19 02:09:48 -0500

    couple of test drivers + DP works (slowly)

--- src/edu/brown/cs/buildingparser/library/Prim2DRects.scala
+++ src/edu/brown/cs/buildingparser/library/Prim2DRects.scala
@@ -3,6 +3,7 @@ package edu.brown.cs.buildingparser.library
 import org.opencv.core.Size
 import org.opencv.core.Rect
 import org.opencv.core.Mat
+import org.opencv.core.CvType
 import org.opencv.core.Scalar
 import java.io.File
 import org.opencv.core.MatOfKeyPoint
@@ -14,6 +15,8 @@ import collection.JavaConverters._
 import org.opencv.features2d.FeatureDetector
 import org.opencv.features2d.DescriptorExtractor
 import org.opencv.core.Point
+
+import edu.brown.cs.buildingparser.Util
 
 class Translate(ofs:Point) extends Transform {
 		def transform(input:Point):Point = {
@@ -31,6 +34,7 @@ class Prim2DRect(val data:Mat, val masks:Mat, nameFormatter:(Size => String)) ex
 			case Some(t) => t.transform(origin)
 			case None => origin
 		}
+		//Console.println("Projecting to ${ofs.x} x ${ofs.y}")
 		
 		((0 until varsStart).map(i => (null, i)) ++ (colorVars zip (varsStart until (varsStart + numColorVars)))).foreach{
 			case(c, i) =>
@@ -40,10 +44,11 @@ class Prim2DRect(val data:Mat, val masks:Mat, nameFormatter:(Size => String)) ex
 					val fullVar = new Mat(masks.size, masks.`type`, new Scalar(i))
 					val varOnly = new Mat
 					Core.compare(masks, fullVar, varOnly, Core.CMP_EQ)
+					//Util.makeImageFrame(Util.matToImage(varOnly), f"mask $i / $varsStart for ${name}")
 					(if(i < varsStart){data}else{new Mat(data.size, data.`type`, c)}, varOnly)
 				}
 				
-				src.copyTo(dst.submat(ofs.x.intValue, (ofs.x + bounds.width).intValue, ofs.y.intValue, (ofs.y + bounds.height).intValue), mask)
+				src.copyTo(dst.submat(ofs.y.intValue, (ofs.y + bounds.height).intValue, ofs.x.intValue, (ofs.x + bounds.width).intValue), mask)
 				
 		}
 	}
@@ -59,7 +64,7 @@ class Prim2DRect(val data:Mat, val masks:Mat, nameFormatter:(Size => String)) ex
 		project(primRepr, colorVars, None)
 		
 		val dstChunk = new Mat(data.size, data.`type`)
-		dst.submat(ofs.x.intValue, (ofs.x + bounds.width).intValue, ofs.y.intValue, (ofs.y + bounds.height).intValue).copyTo(dstChunk)
+		dst.submat(ofs.y.intValue, (ofs.y + bounds.height).intValue, ofs.x.intValue, (ofs.x + bounds.width).intValue).copyTo(dstChunk)
 		
 		val chunkSmooth = new Mat
 		Imgproc.adaptiveBilateralFilter(dstChunk, chunkSmooth, new Size(5, 5), 2)
@@ -85,6 +90,9 @@ class Prim2DRect(val data:Mat, val masks:Mat, nameFormatter:(Size => String)) ex
 }
 
 object Prim2DRectsLib extends PrimLib[Prim2DRect] {
+	def formatterForKind(kind:String):(Size => String) = {
+		((sz:Size) => f"$kind - ${sz.width.intValue}%d x ${sz.height.intValue}%d")
+	}
 	def loadFromDirs(specialKinds:Set[String], srcs:List[File]) : (List[Prim2DRect],Map[String, List[Prim2DRect]]) = {
 		val primImgs = srcs.map{
 			f => 
@@ -98,7 +106,7 @@ object Prim2DRectsLib extends PrimLib[Prim2DRect] {
 		  
 		val allPrims = primImgs.map{
 			case(kind, varsMask, img) =>
-				(kind, new Prim2DRect(img, varsMask, ((sz:Size) => f"$kind - ${sz.width.intValue}%d x ${sz.height.intValue}%d")))
+				(kind, new Prim2DRect(img, varsMask, formatterForKind(kind)))
 		}
 		allPrims.foldLeft((List[Prim2DRect](),Map[String, List[Prim2DRect]]())){
 			case(lastOut, in) =>
@@ -126,5 +134,89 @@ object Prim2DRectsLib extends PrimLib[Prim2DRect] {
 				siftExtractor.compute(grey, kp, desc)
 				(prim -> (kp, desc))
 		}.toMap
+	}
+}
+
+object BrickSynth {
+	def bricksFromDims(dims:List[Size], gridStep:(Int,Int)):List[Prim2DRect] = {
+		dims.map{
+			sz =>
+				val pxWidth = (sz.width * gridStep._1).intValue
+				val pxHeight = (sz.height * gridStep._2).intValue
+				//Console.println(f"Making $pxWidth x $pxHeight (rows: $pxHeight, columns: $pxWidth)")
+				val data = Mat.zeros(pxHeight, pxWidth, CvType.CV_8UC3)
+				val masks = new Mat(pxHeight, pxWidth, CvType.CV_8UC1, new Scalar(2))
+				Core.rectangle(masks, new Point(0,0), new Point(pxWidth - 1, pxHeight - 1), new Scalar(3), 1)
+				new Prim2DRect(data, masks, Prim2DRectsLib.formatterForKind("brick"))
+		}
+	}
+	
+	val PLATE_HEIGHT = 1
+	val BRICK_HEIGHT = 3
+	val BRICK_STEP = (20,8)
+	
+	val STD_DIMS = List((List(1, 2, 3, 4, 6, 8, 10, 12, 16), List(PLATE_HEIGHT, BRICK_HEIGHT)),(List(1,2),List(2*BRICK_HEIGHT, 5*BRICK_HEIGHT)))
+	
+	val COLOR_TABLE = List((new Scalar(0x1D, 0x13, 0x05), new Scalar(0x59, 0x59, 0x59)),
+			(new Scalar(0xBF, 0x55, 0x00), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x3E, 0x7A, 0x25), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x8F, 0x83, 0x00), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x09, 0x1A, 0xC9), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xA0, 0x70, 0xC8), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x27, 0x39, 0x58), new Scalar(0x1E, 0x1E, 0x1E)),
+			(new Scalar(0x9D, 0xA1, 0x9B), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x5C, 0x6E, 0x6D), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xE3, 0xD2, 0xB4), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x4A, 0x9F, 0x4B), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xAF, 0xA5, 0x55), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x5E, 0x70, 0xF2), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xAC, 0x97, 0xFC), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x37, 0xCD, 0xF2), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xFF, 0xFF, 0xFF), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xB8, 0xDA, 0xC2), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x96, 0xE6, 0xFB), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x9E, 0xCD, 0xE4), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xE2, 0xCA, 0xC9), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x7B, 0x00, 0x81), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xB0, 0x32, 0x20), new Scalar(0x1E, 0x1E, 0x1E)),
+			(new Scalar(0x18, 0x8A, 0xFE), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x78, 0x39, 0x92), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x0B, 0xE9, 0xBB), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0x73, 0x8A, 0x95), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xC8, 0xAD, 0xE4), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xBA, 0x78, 0xAC), new Scalar(0x33, 0x33, 0x33)),
+			(new Scalar(0xED, 0xD5, 0xE1), new Scalar(0x33, 0x33, 0x33)))
+	
+	def getStdBricks():List[Prim2DRect] = {
+		STD_DIMS.map{
+			case(widths, heights) =>
+				bricksFromDims(widths.map{
+					width =>
+						heights.map{
+							height =>
+								new Size(width, height)
+						}
+				}.flatten, BRICK_STEP)
+		}.flatten
+	}
+}
+
+object BrickTest {
+	def main(args:Array[String]):Unit = {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+		val primset = BrickSynth.getStdBricks()
+		val C_DIVISOR = 8
+		primset.foreach{
+			prim =>
+				val dst = new Mat(prim.data.height * Math.ceil(BrickSynth.COLOR_TABLE.length / C_DIVISOR.floatValue).intValue, prim.data.width * C_DIVISOR, CvType.CV_8UC3)
+				Console.println(f"dst for ${prim.name} is ${prim.data.width * C_DIVISOR} x ${prim.data.height * (BrickSynth.COLOR_TABLE.length / C_DIVISOR)}")
+				BrickSynth.COLOR_TABLE.zipWithIndex.foreach{
+					case((face_color, edge_color),i) =>
+						Console.println(f"Translate $i to ${(i % C_DIVISOR) * prim.data.width} x ${(i / C_DIVISOR) * prim.data.height}")
+						prim.project(dst, List(face_color, edge_color), Some(new Translate(new Point((i % C_DIVISOR) * prim.data.width,(i / C_DIVISOR) * prim.data.height))))
+				Console.println("test")
+				}
+				Util.makeImageFrame(Util.matToImage(dst), prim.name)
+		}
 	}
 }
--- src/edu/brown/cs/buildingparser/synth/DPEvaluator.scala
+++ src/edu/brown/cs/buildingparser/synth/DPEvaluator.scala
@@ -2,11 +2,18 @@ package edu.brown.cs.buildingparser.synth
 
 import edu.brown.cs.buildingparser.library.Prim2DRect
 import edu.brown.cs.buildingparser.library.Prim2DRectsLib
+import edu.brown.cs.buildingparser.library.BrickSynth
 import org.opencv.core.Mat
 import edu.brown.cs.buildingparser.library.Transform
 import org.opencv.core.Scalar
 import edu.brown.cs.buildingparser.library.Translate
 import org.opencv.core.Point
+
+import org.opencv.highgui.Highgui
+import org.opencv.core.Core
+import org.opencv.imgproc.Imgproc
+import edu.brown.cs.buildingparser.Util
+import edu.brown.cs.buildingparser.training.SamplerMain
 
 
 class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
@@ -17,29 +24,46 @@ class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
 	val notInitializedC:(Double,Option[(List[Scalar],Prim2DRect)]) = (Double.NaN, None)
 	val notInitializedR:(Double,Int) = (Double.NaN, 0)
 	
-	class DPHelper(img:Mat, lib:List[Prim2DRect], colorTable:Set[Scalar], colsTable:Array[Array[Array[(Double,Option[(List[Scalar],Prim2DRect)])]]], rowsTable:Array[(Double,Int)]) {
+	class DPHelper(img:Mat, lib:List[Prim2DRect], colorTable:Set[(Scalar,Scalar)], colsTable:Array[Array[Array[(Double,Option[(List[Scalar],Prim2DRect)])]]], rowsTable:Array[(Double,Int)]) {
+		val heightSetsLE = new Array[Set[Int]](rowsTable.length) 
+		val brickSetsE = new Array[Array[List[Prim2DRect]]](rowsTable.length)
+		Console.println("Initializing height sets")
+		(0 until heightSetsLE.length).foreach{
+			h => 
+				Console.println(f"height $h")
+				heightSetsLE(h) = lib.view.map(brick => brick.data.height / gridStep._2).filter(_ <= h).toSet[Int]
+				brickSetsE(h) = new Array[List[Prim2DRect]](colsTable(h)(h).length)
+				(0 until brickSetsE(h).length).foreach{
+					w =>
+						Console.println(f"  width $w")
+						brickSetsE(h)(w) = lib.view.filter(brick => brick.data.width / gridStep._1 <= w && brick.data.height / gridStep._2 == h).toList
+				}
+		}
+		Console.println("Done Initializing")
+		
 		def optimizeColumns(r:Int, h:Int, w:Int):(Double,Option[(List[Scalar],Prim2DRect)]) = {
 			if(h == 0){
 				throw new IllegalStateException("Tried to consider 0-height primitives")
 			} else if(colsTable(r)(h)(w)._1.isNaN){
+				//Console.println(f"  table cell ($r)($h)($w) is not cached")
 				colsTable(r)(h)(w) = if(w == 0){
 					(0, None)
 				} else {
 					colorTable.view.map{
-						color =>
-							colorTable.view.map{
-								complColor =>	
-									lib.view.filter(brick => brick.data.width / gridStep._1 <= w && brick.data.height / gridStep._2 == h).map{
-										brick => 
-											val colorVars = List(color, complColor)
-											val nextW = w - (brick.data.width / gridStep._1)
-											val brickReward = brick.reward(img, colorVars, Some(new Translate(new Point(nextW * gridStep._1, r * gridStep._2))), params)
-											val restReward = optimizeColumns(r, h, nextW)
-											(brickReward + restReward._1, Some(colorVars, brick))
-									}
-							}.flatten
+						case(color,complColor) =>
+							brickSetsE(h)(w).map{
+								brick => 
+									val colorVars = List(color, complColor)
+									val nextW = w - (brick.data.width / gridStep._1)
+									val brickReward = brick.reward(img, colorVars, Some(new Translate(new Point(nextW * gridStep._1, r * gridStep._2))), params)
+									val restReward = optimizeColumns(r, h, nextW)
+									(brickReward + restReward._1, Some(colorVars, brick))
+							}
 					}.flatten.foldLeft(noSolutionC){
 						case(b, a) =>
+							if(a._1 == b._1){
+								Console.println(f"weird, there's a tie! ${a._1} == ${b._1}")
+							}
 							if(a._1 > b._1){
 								a
 							} else {
@@ -47,16 +71,18 @@ class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
 							}
 					}
 				}
+				//Console.println(f"  table cell ($r)($h)($w) is now cached")
 			}
 			colsTable(r)(h)(w)
 		}
 	
 		def optimizeRows(h:Int):(Double,Int) = {
 			if(rowsTable(h)._1.isNaN){
+				Console.println(f"table row $h is not cached")
 				rowsTable(h) = if(h == 0){
 					(0, 0)
 				} else {
-					lib.view.map(brick => brick.data.height / gridStep._2).filter(_ <= h).toSet[Int].map{
+					heightSetsLE(h).map{
 						brickH =>
 							val rowReward = optimizeColumns(h - brickH, brickH, img.width / gridStep._1)
 							val restReward = optimizeRows(h - brickH)
@@ -64,6 +90,9 @@ class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
 							(rowReward._1 + restReward._1, brickH)
 					}.foldLeft(noSolutionR){
 						case(b, a) =>
+							if(a._1 == b._1){
+								Console.println(f"weird, there's a tie! ${a._1} == ${b._1}")
+							}
 							if(a._1 > b._1){
 								a
 							} else {
@@ -71,11 +100,13 @@ class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
 							}
 					}
 				}
+				Console.println(f"table row $h is now cached")
 			}
 			rowsTable(h)
 		}	
 		
 		def unpackRow(r:Int, h:Int, c:Int):Stream[(Transform,List[Scalar],Prim2DRect)] = {
+			Console.println(f"Unpacking row: ($r)($h)($c)")
 			val (bestReward, right) = optimizeColumns(r, h, c)
 			right match{
 				case Some(primSpec) => 
@@ -91,6 +122,7 @@ class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
 		}
 		
 		def unpackTable(bottomR:Int):Stream[(Transform,List[Scalar],Prim2DRect)] = {
+			Console.println(f"Unpacking table: ($bottomR)")
 			val (bestReward, h) = optimizeRows(bottomR)
 			val r = bottomR - h
 			if(r == 0 && h == 0){
@@ -103,19 +135,56 @@ class DPEvaluator(gridStep:(Int, Int), k:Double = 1, l:Double = 0.25) {
 		}
 	}
 	
-	def evaluate(img:Mat, lib:List[Prim2DRect], colorTable:Set[Scalar]):Stream[(Transform,List[Scalar],Prim2DRect)] = {
+	def evaluate(img:Mat, lib:List[Prim2DRect], colorTable:Set[(Scalar,Scalar)]):Stream[(Transform,List[Scalar],Prim2DRect)] = {
 		(lib.view.map(prim => prim.data) :+ img).foreach{
 			mat => if(mat.width % gridStep._1 != 0 || mat.height % gridStep._2 != 0){
-				throw new IllegalStateException("Can't run DP unless all image sizes are multiples of " + gridStep)
+				throw new IllegalStateException(f"Can't run DP unless all image sizes are multiples of $gridStep but got (${mat.width}, ${mat.height}) instead")
 			}
 		}
 		val steppedRowCount = img.height / gridStep._2
 		val steppedColCount = img.width / gridStep._1
 		val colsTable = Array.fill[(Double,Option[(List[Scalar],Prim2DRect)])](steppedRowCount + 1, steppedRowCount + 1, steppedColCount + 1)(notInitializedC)
 		val rowsTable = Array.fill[(Double,Int)](steppedRowCount + 1)(notInitializedR)
+		Console.println(f"rows table has ${rowsTable.length} entries")
+		Console.println(f"cols table has ${(steppedRowCount + 1) * (steppedRowCount + 1) * (steppedColCount + 1)} entries")
+		Console.println(f"${colorTable.size * lib.length} brick / color combos")
 	
-		val helper = new DPHelper(img:Mat, lib:List[Prim2DRect], colorTable:Set[Scalar], colsTable, rowsTable)
+		val helper = new DPHelper(img:Mat, lib:List[Prim2DRect], colorTable:Set[(Scalar,Scalar)], colsTable, rowsTable)
 		helper.unpackTable(img.height / gridStep._2)
+	}	
+}
+
+object DPTest {
+	def main(args:Array[String]):Unit = {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+		SamplerMain.isMain = false
+		val grazSampler = SamplerMain.grazSampler
+		
+		val srcHandle = grazSampler.imgHandleFromName("facade_1_0056092_0056345.png")//("facade_0_0099003_0099285.png")//
+		//val labelHandle = grazSampler.pairedImgs(srcHandle)
+		//val (srcBase, examples) = grazSampler.extractOneExampleSet(srcHandle, labelHandle)
+		val srcImg = Highgui.imread(srcHandle.getAbsolutePath, Highgui.CV_LOAD_IMAGE_COLOR)
+		val gridRows = LDrawGridify.nearestMeshPoint(srcImg.rows, LDrawGridify.gridStep._2)
+		val gridCols = LDrawGridify.nearestMeshPoint(srcImg.cols, LDrawGridify.gridStep._1)
+		
+		val gridImg = new Mat(gridRows, gridCols, srcImg.`type`)
+		Imgproc.resize(srcImg, gridImg, gridImg.size)
+		Console.println(f"Resizing ${srcImg.cols} x ${srcImg.rows} to $gridCols x $gridRows for ${LDrawGridify.gridStep}")
+		
+		val brickPlacer = new DPEvaluator(LDrawGridify.gridStep)
+		val instr = brickPlacer.evaluate(gridImg, BrickSynth.getStdBricks(), BrickSynth.COLOR_TABLE.toSet[(Scalar,Scalar)])
+		
+		val dstImg = Mat.zeros(gridImg.size, gridImg.`type`)
+		instr.foreach{
+			case(proj,colors,brick) =>
+				brick.project(dstImg, colors, Some(proj))
+		}
+		
+		Util.makeImageFrame(Util.matToImage(srcImg), "src")
+		Util.makeImageFrame(Util.matToImage(gridImg), "stretched to grid")
+		Util.makeImageFrame(Util.matToImage(dstImg), "bricked over")
+		
+		
+		
 	}
-	
 }
